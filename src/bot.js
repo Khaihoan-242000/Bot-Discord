d5a6c0b0637e0754e273870c3916b99c4c181288
Lê Khải Hoàn <75442024+Khaihoan-242000@users.noreply.github.com>
2020-12-04 15:15:13 +0700

    add time function

--- src/bot.js
+++ src/bot.js
@@ -12,6 +12,19 @@ client.on('ready', () => {
   command(client, ['ping', 'test'], (message) => {
     message.channel.send('Pong!');
   });
+  // Command Time
+  command(client, ['time'], message => {
+    const dateTime = new Date();
+    const day = dateTime.getDay();
+    const month = dateTime.getMonth();
+    const years = dateTime.getFullYear();
+    const hour = dateTime.getHours();
+    const minute = dateTime.getMinutes();
+    const second = dateTime.getSeconds();
+
+    message.channel.send(`Today: ${day}/${month}/${years} | ${second}:${minute}:${hour}`)
+
+  });
 });
 
 client.login(token);
